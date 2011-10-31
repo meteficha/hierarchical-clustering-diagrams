b247b232ddf4a3ba08bc3f09976d209cefa1f618
Felipe Lessa <felipe.lessa@gmail.com>
2011-10-31 13:16:38 +0000

    Initial import.

---
+++ src/Diagrams/Dendrogram.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE DeriveDataTypeable, FlexibleContexts #-}
+
+module Diagrams.Dendrogram
+    ( dendrogram
+    ) where
+
+-- from base
+import Data.Typeable (Typeable)
+
+-- from hierarachical-clustering
+import Data.Clustering.Hierarchical (Dendrogram(..))
+
+-- from diagrams-lib
+import Diagrams.Prelude
+
+
+dendrogram :: Renderable (Path R2) b =>
+              (t -> AnnDiagram b R2 Any)
+           -> Dendrogram t
+           -> AnnDiagram b R2 Any
+dendrogram drawItem = lw 0.1 . fst . drawDendro
+  where
+    drawDendro (Leaf a) = (dia, 0)
+        where
+          dia = drawItem a # alignT
+    drawDendro (Branch d l r) = (dia, d)
+        where
+          (diaL, posL) = drawDendro l
+          (diaR, posR) = drawDendro r
+
+          line pos name = vrule (d - pos) # alignT # named name
+          diaL' = line posL L === (D |> diaL)
+          diaR' = line posR R === (D |> diaR)
+          horizLine = withName L $ \(pl, _) ->
+                      withName R $ \(pr, _) ->
+                          stroke (pl ~~ pr) # atop
+
+          dia = horizLine (diaL' # alignT ||| diaR' # alignT)
+
+data Discard = D deriving (Typeable, Show, Ord, Eq)
+instance IsName Discard where
+
+data ConnectingPoint = L | R deriving (Typeable, Show, Ord, Eq)
+instance IsName ConnectingPoint where
---
+++ tests/runtests.hs
@@ -0,0 +1,40 @@
+-- from hierarchical-clustering
+import Data.Clustering.Hierarchical
+
+-- from diagrams-lib
+import Diagrams.Prelude
+
+-- from diagrams-cairo
+import Diagrams.Backend.Cairo (Cairo)
+import Diagrams.Backend.Cairo.CmdLine (multiMain)
+
+-- from this package
+import qualified Diagrams.Dendrogram as D
+
+main :: IO ()
+main = multiMain $ [ ("test", D.dendrogram char test) ] ++
+                   [ ("alpha-" ++ n, D.dendrogram char (alpha l))
+                     | (n,l) <- [ ("single",   SingleLinkage)
+                                , ("complete", CompleteLinkage)
+                                , ("clink",    CLINK)
+                                , ("upgma",    UPGMA)
+                                ]
+                   ]
+
+
+char :: Char -> Diagram Cairo R2
+char c = pad 1.3 $ roundedRect (1,1) 0.1 `atop` text [c]
+
+test :: Dendrogram Char
+test = Branch 5
+         (Branch 2
+           (Branch 1
+             (Leaf 'A')
+             (Leaf 'B'))
+           (Leaf 'C'))
+         (Leaf 'D')
+
+alpha :: Linkage -> Dendrogram Char
+alpha link = dendrogram link ['A'..'Z'] dist
+    where
+      dist a b = fromIntegral $ abs (fromEnum a - fromEnum b)
