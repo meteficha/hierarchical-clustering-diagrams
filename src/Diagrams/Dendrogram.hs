1ee9b7812cc48f4ddcae03da2ce802f649570fdb
Felipe Lessa <felipe.lessa@gmail.com>
2011-11-01 02:01:39 +0000

    Draw the dendrogram using a single path.

--- src/Diagrams/Dendrogram.hs
+++ src/Diagrams/Dendrogram.hs
@@ -1,14 +1,14 @@
-{-# LANGUAGE DeriveDataTypeable, FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Diagrams.Dendrogram
     ( dendrogram
     ) where
 
 -- from base
-import Data.Typeable (Typeable)
+import Control.Arrow (second)
 
 -- from hierarachical-clustering
-import Data.Clustering.Hierarchical (Dendrogram(..))
+import Data.Clustering.Hierarchical (Dendrogram(..), elements)
 
 -- from diagrams-lib
 import Diagrams.Prelude
@@ -16,31 +16,38 @@ import Diagrams.Prelude
 
 -- | @dendrogram drawItem dendro@ is a drawing of the dendrogram
 -- @dendro@ using @drawItem@ to draw its leafs.
-dendrogram :: Renderable (Path R2) b =>
-              (t -> AnnDiagram b R2 Any)
-           -> Dendrogram t
-           -> AnnDiagram b R2 Any
-dendrogram drawItem = lineCap LineCapSquare . fst . drawDendro
+dendrogram :: (Monoid m, Renderable (Path R2) b) =>
+              (a -> AnnDiagram b R2 m)
+              -> Dendrogram a
+              -> AnnDiagram b R2 m
+dendrogram drawItem dendro = (stroke path_ # value mempty) === items
   where
-    drawDendro (Leaf a) = (dia, 0)
+    drawItems d = hcat [drawItem a # alignT # named n | (a, n) <- elements d]
+
+    dendroPath (Leaf (_, n))  = (mempty, getPos n)
+    dendroPath (Branch d l r) = (path <> pathL <> pathR, pos)
         where
-          dia = drawItem a # alignT
-    drawDendro (Branch d l r) = (dia, d)
-        where
-          (diaL, posL) = drawDendro l
-          (diaR, posR) = drawDendro r
+          (pathL, P (xL, yL)) = dendroPath l
+          (pathR, P (xR, yR)) = dendroPath r
 
-          line pos name = vrule (d - pos) # alignT # named name
-          diaL' = line posL L === (D |> diaL)
-          diaR' = line posR R === (D |> diaR)
-          horizLine = withName L $ \(pl, _) ->
-                      withName R $ \(pr, _) ->
-                          stroke (pl ~~ pr) # atop
+          path = fromVertices [ P (xL, yL)
+                              , P (xL, d+y0)
+                              , P (xR, d+y0)
+                              , P (xR, yR)]
+          pos  = P ((xL + xR) / 2, d+y0)
 
-          dia = horizLine (diaL' # alignT ||| diaR' # alignT)
+    named_     = nameDendrogram dendro
+    items      = drawItems named_
+    names_     = names items
+    getPos n   = let Just [(p, _)] = lookupN n names_ in p
+    (path_, _) = dendroPath named_
 
-data Discard = D deriving (Typeable, Show, Ord, Eq)
-instance IsName Discard where
+    Just [(P (_, y0), _)] = lookupN (0 :: Int) names_
 
-data ConnectingPoint = L | R deriving (Typeable, Show, Ord, Eq)
-instance IsName ConnectingPoint where
+nameDendrogram :: Dendrogram a -> Dendrogram (a, Int)
+nameDendrogram = snd . go 0
+    where
+      go n (Leaf a) = n `seq` (n+1, Leaf (a,n))
+      go n (Branch d l r) =
+          let (n', l') = go n l
+          in second (Branch d l') (go n' r)
